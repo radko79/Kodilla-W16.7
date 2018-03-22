3e62627670108be512177f8b6f9666fcbb875ed5
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-03-22 12:41:14 +0000

    install uuid

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 // entry file
+
+import React from 'react';
+import React-DOM from 'react-DOM';
+import app from './containers/app';
+
+ReactDOM.render (
+	<App/>,
+	document.getElementById('app')
+);
